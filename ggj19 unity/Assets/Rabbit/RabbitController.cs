b93eb21fd3b3da122e0bbe45e586fbe450ea6c79
Rüdiger Brandis <post@rbrandis.eu>
2019-01-26 11:21:45 +0100

    Rabbit Third Person Controller
    Its in the Rue Scene ... no Prefab yet

---
+++ ggj19 unity/Assets/Rabbit/RabbitController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RabbitController : MonoBehaviour
+{
+    public Animator m_Animator; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W)){
+            m_Animator.SetBool("isRunning", true);
+        }
+        else
+        {
+            m_Animator.SetBool("isRunning", false);
+        }
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            m_Animator.SetBool("isJumping", true);
+        }
+        else
+        {
+            m_Animator.SetBool("isJumping", false);
+        }
+
+    }
+}
